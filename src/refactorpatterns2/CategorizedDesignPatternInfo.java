354d76ad8676b808a560f74cde1c5937ae437d01
dart1277 <dart127@o2.pl>
2018-11-05 21:14:30 +0100

    refactored code

---
+++ src/refactorpatterns2/CategorizedDesignPatternInfo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorpatterns2;
+
+/**
+ *
+ * @author cx
+ */
+// package private (intentionally)
+abstract class CategorizedDesignPatternInfo implements CategorizedDesignPattern {
+
+    @Override
+    public boolean areSameType(CategorizedDesignPattern pattern1) {
+        return patternType().equals(pattern1.patternType());
+    }
+
+    abstract DesignPatternInfo getDesignPatternInfo(); // package private
+
+}
